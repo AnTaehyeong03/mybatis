92e086bfec51c25892ce8d03b7f7e97c50a96253
AnTaehyeong03 <ta0313@naver.com>
2025-06-27 19:40:30 +0900

    6월 27일 수업 내용

--- src/main/java/com/kh/mybatis/student/model/service/StudentService.java
+++ src/main/java/com/kh/mybatis/student/model/service/StudentService.java
@@ -1,6 +1,12 @@
 package com.kh.mybatis.student.model.service;
 
+import com.kh.mybatis.student.model.vo.Student;
+
 public interface StudentService {
 
+	int insertStudent(Student s);
 
+	Student selectOneStudent(int no);
+
+	int selectStudentCount();
 }
